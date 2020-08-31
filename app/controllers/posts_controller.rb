d46cc2e8212dbf4fe4ced6094ff08bad906462fb
uchigakiDaiju <uchigaki.daiju@gmail.com>
2020-08-31 14:59:27 +0900

    first commit

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,12 @@
+class PostsController < ApplicationController
+  def index #indexアクションを定義した
+    @posts = Post.all  # 全てのレコードを@postsに代入
+  end
+
+  def new
+  end
+
+  def create
+    Post.create(content: params[:content])
+  end
+end
